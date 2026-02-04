a038370d22ddb04f9974d43be2fabfdc42cc2096
KerubinDev <kelvin.moraes117@gmail.com>
2026-02-03 22:56:09 -0300

    fix: update version in tests to 1.1.0

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,6 +1,6 @@
 def test_version():
     from akita import __version__
-    assert __version__ == "0.1.1"
+    assert __version__ == "1.1.0"
 
 def test_cli_import():
     from akita.cli.main import app
